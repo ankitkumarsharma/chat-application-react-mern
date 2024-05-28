7a585dc67d77795ad46480065a891d57d381dcdc
ankitkumarsharma <aksharma.dixit@gmail.com>
2024-05-29 00:48:59 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            Ankit
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
