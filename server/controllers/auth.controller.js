b8246c88ab0d93f1a749c4808618c2317f3515b7
ankitkumarsharma <aksharma.dixit@gmail.com>
2024-06-09 13:28:15 +0530

    add jwt in app and login api created

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,32 @@
 import User from '../models/user.models.js';
 import bcrypt from 'bcryptjs';
-export const login = (req, res) => {
-    res.send("Welcome to login");
+import generateTokenAndSetCookie from '../utils/generateToken.js';
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if(!user){
+            return res.status(404).json({ error: 'User Not Found!'});
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({ error: 'Invalid Username or Password!'});
+        }
+        generateTokenAndSetCookie(user._id, res);
+        res.status(200).json({
+            id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+            gender: user.gender,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Internal Server Error!" });
+    }
 }
 
 export const logout = (req, res) => {
@@ -34,6 +59,7 @@ export const signup = async (req, res) => {
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         });
         if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
             await newUser.save();
             res.status(201).json({
                 _id: newUser._id,
